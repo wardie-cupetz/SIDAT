ae4471a384444e8cde9bb77b45e869de230d3396
wardie-cupetz <wardiecupetz@gmail.com>
2026-08-31 13:37:03 +0700

    Update supabase-config.js

--- www/js/supabase-config.js
+++ www/js/supabase-config.js
@@ -5,6 +5,20 @@
 const SUPABASE_URL = "https://pxezuuumgzdzdiqsspig.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_i--xXc0Jso51OAYj8Vy92g_OVIt8e3x";
+
+
+const supabaseClient =
+    supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY,
+        {
+            auth: {
+                persistSession: true,
+                autoRefreshToken: true,
+                detectSessionInUrl: false
+            }
+        }
+    );
 // ======================================
 // SIDAT APP VERSION
 // ======================================
